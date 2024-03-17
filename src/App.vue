d86e21d7bffb59f441aef0573e3fdd39196a3d56
Muhamad Arsaludin <muhamad.arsaludin@GDN-MAC-220.local>
2024-03-17 21:56:17 +0700

    Setup project

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <header></header>
+  <body>
+    <router-view></router-view>
+  </body>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
